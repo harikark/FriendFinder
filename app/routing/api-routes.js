09ed0b8bb737d725aafd3493890c07b400713f09
Harika K <harika.kasireddy@gmail.com>
2016-11-03 13:00:53 -0400

    initial commit

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,1 +1,38 @@
-ddddd
+var friends = require('../data/friends.js');
+
+module.exports = function(app) {
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+
+	app.post('/api/friends', function(req, res){
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDifference: 1000
+		};
+		var userData = req.body;
+		var userName = userData.name;
+		var userPhoto = userData.photo;
+		var userScores = userData.scores;
+		var totalDifference = 0;
+
+		//loop through scores of survery
+		for (var i = 0; i < friends.length; i++){
+			console.log(friends[i].name);
+			totalDifference = 0;
+			// loop throught to calculate difference
+			for ( var j =0; j < 10; j++) {
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+					if (totalDifference <= bestMatch.friendDifference){
+						bestMatch.name = friends[i].name;
+						bestMatch.photo = friends[i].photo;
+						bestMatch.friendDifference = totalDifference;
+					}
+			}
+		}	
+
+		friends.push(userData);
+		res.json(bestMatch);
+	});
+};
