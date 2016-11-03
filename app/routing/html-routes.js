ae1b76bb4174e3ed6c3dfd8a4b8c478953b471be
Harika K <harika.kasireddy@gmail.com>
2016-11-03 13:01:40 -0400

    add html-routes.js

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,1 +1,12 @@
-cccccc
+var path = require('path');
+
+module.exports = function(app) {
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	//if none default home
+	app.use(function(req,res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
